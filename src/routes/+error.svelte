fb05adc1ffc9a476d8e086c1d90428440ef28a90
danil <61111955+danilwhale@users.noreply.github.com>
2024-06-07 19:45:54 +0300

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div class="place-content-center w-full h-full">
+    <p class="text-3xl text-center">404!!!</p>
+    <p class="text-xl text-center m-5">page has been exploded!!!</p>
+    <div class="flex justify-center">
+        <img src="/existential_dread.gif" alt="explod">
+    </div>
+</div>
