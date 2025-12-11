0d8fab736e781530159245f2f6c2c04c40526985
danil <61111955+danilwhale@users.noreply.github.com>
2025-12-11 12:14:35 +0200

    d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,24 +8,24 @@
 <ul>
     <li>
         <a href="https://github.com/danilwhale/sharpcraft">sharpcraft</a>
-        <i>- mixed; yet another ancient Minecraft clone, made with C#</i>
+        <i>- dead; yet another ancient Minecraft clone, made with C#</i>
         <ul>
-            <li>sharpcraft - unsupported, c0.0.11a; was made when learning raylib, buggy and laggy</li>
+            <li>sharpcraft - archived, c0.0.11a; was made when learning raylib, buggy and laggy</li>
             <li>sharpcraft-indev - archived, in-20100223; never saw the light of the day; made with raylib</li>
             <li>sharpcraft-2 - hiatus, in-20100223; clean room approach; made with OpenGL (<a href="https://github.com/dotnet/Silk.NET">Silk.NET</a>) and SDL3 (<a href="https://github.com/ppy/SDL3-CS">ppy.SDL3-CS</a>)</li>
         </ul>
     </li>
     <li>
         <a href="https://github.com/danilwhale/Raylib-cs.BleedingEdge">Raylib-cs.BleedingEdge</a>
-        <i>- active; C# bindings for the master branch of <a href="https://github.com/raysan5/raylib">raylib</a></i>
+        <i>- dead; C# bindings for the master branch of <a href="https://github.com/raysan5/raylib">raylib</a></i>
     </li>
     <li>
         <a href="https://github.com/danilwhale/SillyRendering">sillyrendering</a>
-        <i>- unsupported; a Minecraft mod that makes your game renderer silly!</i>
+        <i>- dead; a Minecraft mod that makes your game renderer silly!</i>
     </li>
     <li>
         <a href="https://github.com/danilwhale/BuildingGame">building game</a>
-        <i>- unsupported; a game where you can place tiles, made to learn raylib and C#, now it's just a relic of the past</i>
+        <i>- dead; a game where you can place tiles, made to learn raylib and C#, now it's just a relic of the past</i>
         <ul>
             <li>/main - buggy rewrite of the original branch</li>
             <li>/legacy - the original branch, a lot of obscure code</li>
@@ -34,4 +34,4 @@
         </ul>
     </li>
 </ul>
-<p>you can join <a href="https://discord.com/invite/tAZR5W3tSd">my discord server</a> if you're interested in my projects <i>(such as sharpcraft-2)</i></p>
+<p>you can join <a href="https://discord.com/invite/tAZR5W3tSd">my discord server</a> if you're interested in my projects</p>
